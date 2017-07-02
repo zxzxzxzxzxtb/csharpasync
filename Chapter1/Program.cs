d0d483a334126bf15af1d28f3a704cd79c2c887c
zxzxzxzxzxtb <306578195@qq.com>
2017-07-02 10:27:58 +0800

    create project,first submit

---
+++ Chapter1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            ExceptionTest();
+            Console.Read();
+        }
+
+        static async Task DosomethingAsync() {
+            int val = 13;
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            val *= 2;
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            Console.WriteLine(val);
+        }
+
+        void RotateMatrices(IEnumerable<Martix> matrices, float degrees) {
+            Parallel.ForEach(matrices, m => m.Rotate(degrees));
+        }
+
+        IEnumerable<bool> PrimalityTest(IEnumerable<int> values) {
+            return values.AsParallel().Select(val => IsPrime(val));
+        }
+
+        private bool IsPrime(int val) {
+            return true;
+        }
+
+        void ProcessArray(double[] array) {
+            Parallel.Invoke(
+                ()=> ProcessPartialArray(array,0,array.Length /2),
+                ()=> ProcessPartialArray(array,array.Length/2,array.Length)
+            );
+        }
+
+        void ProcessPartialArray(double[] array,int begin,int end) {
+
+        }
+
+       static void ExceptionTest() {
+            try {
+                Parallel.Invoke(() => { throw new Exception(); }, () => { throw new Exception(); });
+            }catch(AggregateException ex) {
+                ex.Handle(e => {
+                    Console.WriteLine(e.Message);
+                    return true;
+                });
+            }
+        }
+    }
+
+    internal class Martix
+    {
+        internal void Rotate(float degrees) {
+            // do something
+        }
+    }
+}
---
+++ Chapter2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            //Task<string> slt = DelayResult<string>("hello", TimeSpan.FromSeconds(2));
+            //Console.WriteLine(slt.Result);
+            Task<string> slt = DownloadStringWithRetries("http://www.ofmonkey2.com");
+            Console.WriteLine(slt);
+            Console.Read();
+        }
+
+        static async Task<T> DelayResult<T>(T result, TimeSpan delay) {
+            await Task.Delay(delay);
+            return result;
+        }
+
+        static async Task<string> DownloadStringWithRetries(string uri) {
+            using (var client = new HttpClient()) {
+                var nextDelay = TimeSpan.FromSeconds(1);
+                for (int i = 0; i != 3; ++i) {
+                    try {
+                        return await client.GetStringAsync(uri);
+                    } catch {
+
+                    }
+                    await Task.Delay(nextDelay);
+                    nextDelay = nextDelay + nextDelay;
+                }
+                return await client.GetStringAsync(uri);
+            }
+        }
+    }
+}
