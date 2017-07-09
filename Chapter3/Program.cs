69bbf4edd29dc87ff5dc4d74912e7a1f22763779
zxzxzxzxzxtb <306578195@qq.com>
2017-07-09 16:46:43 +0800

    chapter3 completed

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -96,5 +97,87 @@ namespace Chapter3
                 );
         }
         #endregion
+
+        #region 3.3并行调用
+        static void ProcessArray(double[] array) {
+            Parallel.Invoke(
+                () => ProcessPartialArray(array, 0, array.Length /2),
+                () => ProcessPartialArray(array, array.Length/2, array.Length)
+            );
+        }
+
+        static void ProcessPartialArray(double[] array, int begin, int end) {
+
+        }
+
+        static void DoAction20Times(Action action) {
+            Action[] actions = Enumerable.Repeat(action, 20).ToArray();
+            Parallel.Invoke(actions);
+        }
+
+        static void DoActions20Times(Action action, CancellationToken token) {
+            Action[] actions = Enumerable.Repeat(action, 20).ToArray();
+            Parallel.Invoke(new ParallelOptions { CancellationToken = token }, actions);
+        }
+        #endregion
+
+        #region 3.4动态并行
+        public void ProcessTree(Node root) {
+            var task = Task.Factory.StartNew(() => Traverse(root),
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskScheduler.Default
+            );
+            task.Wait();
+        }
+
+        private void Traverse(Node current) {
+            if (current.Left != null) {
+                Task.Factory.StartNew(()=>Traverse(current.Left),
+                    CancellationToken.None,
+                    TaskCreationOptions.AttachedToParent,
+                    TaskScheduler.Default
+                );
+            }
+            if (current.Right != null) {
+                Task.Factory.StartNew(() => Traverse(current.Right),
+                    CancellationToken.None,
+                    TaskCreationOptions.AttachedToParent,
+                    TaskScheduler.Default
+                );
+            }
+        }
+
+        void TaskContinuation() {
+            Task task = Task.Factory.StartNew(
+                    () => Thread.Sleep(TimeSpan.FromSeconds(2)),
+                    CancellationToken.None,
+                    TaskCreationOptions.None,
+                    TaskScheduler.Default
+                );
+            Task continuation = task.ContinueWith(
+                    t => Trace.WriteLine("Task is done"),
+                    CancellationToken.None,
+                    TaskContinuationOptions.None,
+                    TaskScheduler.Default
+                );
+        }
+        #endregion
+
+        #region 3.5并行LINQ
+        static IEnumerable<int> MultiyBy2(IEnumerable<int> values) {
+            return values.AsParallel().AsOrdered().Select(item => item * 2);
+        }
+
+        static int ParallelSum(IEnumerable<int> values) {
+            return values.AsParallel().Sum();
+        }
+        #endregion
+    }
+
+    class Node{
+        public Node Left{get;set;}
+
+        public Node Right{get;set;}
     }
 }
